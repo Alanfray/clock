b1fdef43713252b5d0171d9572dcdbdd3ecb5019
alanfray <1421803126@qq.com>
2019-10-10 17:31:56 +0800

    build

---
+++ build/precache-manifest.8f0a87a66a1ca86359cd1e14523d3525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27db82cd9c8a4803b5f6ec9ac63d6873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad9c5c2892e4c55ad72",
+    "url": "/static/css/main.52595251.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "4ad9c5c2892e4c55ad72",
+    "url": "/static/js/main.bee95c44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db806eef73f72cc58db144aadbb6c689",
+    "url": "/static/media/2.db806eef.jpg"
+  }
+]);
